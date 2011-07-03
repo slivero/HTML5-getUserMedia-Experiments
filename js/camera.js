47bccc838511c6c343d3e18ce129749e69f37fca
Oliver Smith <chemicaloli@gmail.com>
2011-07-03 19:37:49 +0100

    Tidy Tidy Tidy...

--- js/camera.js
+++ js/camera.js
@@ -1,25 +1,20 @@
-
-
 var videoId = 'video';
-var scaleFactor = 1;
-var snapshots = [];
+var scaleFactor = 0.5;
+var snapshot = null;
             
 var video = document.getElementsByTagName('video')[0], 
 heading = document.getElementsByTagName('h1')[0],
 button = document.getElementById('shoot');
 
 /**
-             * Captures a image frame from the provided video element.
-             * 
-             * @param {Video} video HTML5 video element from where the image frame will be captured.
-             * @param {Number} scaleFactor Factor to scale the canvas element that will be return. This is an optional parameter.
-             * 
-             * @return {Canvas}
-             */
+ * Captures a image frame from the provided video element.
+ * 
+ * @param {Video} video HTML5 video element from where the image frame will be captured.
+ * @param {Number} scaleFactor Factor to scale the canvas element that will be return. This is an optional parameter.
+ * 
+ * @return {Canvas}
+ */
 function capture(video, scaleFactor) {
-    if(scaleFactor == null){
-        scaleFactor = 1;
-    }
     var w = video.videoWidth * scaleFactor;
     var h = video.videoHeight * scaleFactor;
     var canvas = document.createElement('canvas');
@@ -31,43 +26,63 @@ function capture(video, scaleFactor) {
 }
             
 /**
-             * Invokes the <code>capture</code> function and attaches the canvas element to the DOM.
-             */
+ * Invokes the capture() function and attaches the canvas element to the DOM.
+ */
 function shoot(){
     var output = document.getElementById('output');
     var canvas = capture(video, scaleFactor);
     canvas.onclick = function(){
         window.open(this.toDataURL());
     };
-    snapshots.unshift(canvas);
-    output.innerHTML = '';
     
+    //load the photo when the new page opens
     $('#photos').live('pageshow',function(){
-        for(var i=0; i<4; i++){
-            output.appendChild(snapshots[i]);
-        }
+        $('#output').html(canvas);
     });
     
+    //load the photos page
     $.mobile.changePage('#photos');
-    
-    for(var i=0; i<4; i++){
-        output.appendChild(snapshots[i]);
-    }
-    
+
 }
-            
+    
+/**
+ * Tests support for toDataURL required for linking to full size images
+ * 
+ * @return {bool}
+ */
+    
 function supportsToDataURL()
-{
-                
+{        
     var c = document.createElement("canvas");
     var data = c.toDataURL("image/png");
     return (data.indexOf("data:image/png") == 0);
 }
-            
-//check if there is videocapture functionality (sometimes flakey)
-if(navigator.getUserMedia) 
+
+/**
+ * Tests support for getUserMedia... kind of the whole point of this
+ * 
+ * @return {bool}
+ */
+
+function supportsGetUserMedia()
 {
-                
+    if(!navigator.getUserMedia) 
+    {
+        heading.textContent = 
+        "Native web camera streaming is not supported in this browser!";
+        
+        return false
+    }
+    else
+    {
+        return true;
+    }
+}
+
+
+            
+if(supportsGetUserMedia()) 
+{      
     navigator.getUserMedia('video', successCallback, errorCallback);
                 
     function successCallback( stream ) 
@@ -81,11 +96,7 @@ if(navigator.getUserMedia)
         "An error occurred: [CODE " + error.code + "]";
     }
 } 
-else 
-{
-    heading.textContent = 
-        "Native web camera streaming is not supported in this browser!";
-}
+
             
 if(!supportsToDataURL())
 {
